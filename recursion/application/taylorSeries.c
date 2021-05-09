69b4253c7ef5c02d0ac6f6fb7b5fbae2e8e84dd2
gokulmaxi <gokulakams@gmail.com>
2021-05-09 11:51:08 +0530

    added taylor series calculation using horners rule

--- recursion/application/taylorSeries.c
+++ recursion/application/taylorSeries.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
- * calculates taylor series values upto n
+ * calculates taylor series values upto n using normal recursion
  * @param x value of x 
  * @param n value of n th term 
  * @result value of e^x 
 */
-double taylorSeries(int x , int n){
+double taylorSeriesN(int x , int n){
     static double p=1,f=1;
     double r;
     if (n!=0)
     {
-        r=taylorSeries(x,n-1);
+        r=taylorSeriesN(x,n-1);
         p=p*x;
         f=f*n;
         return r+p/f;
@@ -21,10 +21,29 @@ double taylorSeries(int x , int n){
     }
     
 }
+/**
+ * calculates taylor series values upto n using horners rule
+ * @param x value of x 
+ * @param n value of n th term 
+ * @result value of e^x 
+*/
+double taylorSeriesR(int x, int n){
+    static double s;
+    if (n==0)
+    {
+        return s;
+    }
+    else
+    {
+        s=1+x*s/n;
+        return taylorSeriesR(x,n-1);
+    }
+    
 
+}
 int main(int argc, char const *argv[])
 {
-    double result=taylorSeries(4,50);
+    double result=taylorSeriesR(4,50);
     printf("%lf \n",result);   // O\P = 54.598150
     return 0;
 }
